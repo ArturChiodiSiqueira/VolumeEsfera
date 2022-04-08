6c95338e6448c05e3eeac65ba00c8310443154bf
ArturChiodiSiqueira <arturchiodi@gmail.com>
2022-04-08 17:34:42 -0300

    Add project files.

---
+++ VolumeEsfera/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VolumeEsfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume;
+            Console.WriteLine("PROGRAMA PARA CALCULAR O VOLUME DE UMA ESFERA\n");
+            Console.WriteLine("informe o raio (r) da esfera:");
+            raio = double.Parse(Console.ReadLine());
+            if (raio < 0)
+            {
+                Console.WriteLine("\nNÃO EXISTE VOLUME NEGATIVO!");
+            }
+            else
+            {
+                volume = (4 / 3.0) * Math.PI * Math.Pow(raio, 3);
+
+                Console.WriteLine("\nO VOLUME DA ESFERA É: " + volume.ToString("F3"));
+            }
+        }
+    }
+}
